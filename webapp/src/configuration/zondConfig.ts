0e5bfe5d81c32562e1140896a16d5e8ad996836e
Harald Langmar <harald@langmar.nl>
2024-11-17 11:38:48 +0100

    Re-add the config..

---
+++ webapp/src/configuration/zondConfig.ts
@@ -0,0 +1,17 @@
+export const ZOND_PROVIDER = {
+  DEV: { 
+    id: "DEV",
+    url: "http://localhost:8545",
+    name: "Zond Local Node"
+  },
+  TEST_NET: {
+    id: "TEST_NET",
+    url: "http://209.250.255.226:8545",
+    name: "Zond Testnet"
+  },
+  MAIN_NET: {
+    id: "MAIN_NET",
+    url: "http://mainnet.zond.network:8545",
+    name: "Zond Mainnet"
+  }
+};
