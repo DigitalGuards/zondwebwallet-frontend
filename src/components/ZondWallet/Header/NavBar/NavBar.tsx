fde77a38da81181e9c1c893bb1fdafc75ac2e412
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2025-01-28 09:51:08 +0900

    feat: update navbar

---
+++ src/components/ZondWallet/Header/NavBar/NavBar.tsx
@@ -0,0 +1,59 @@
+import { observer } from "mobx-react-lite";
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from "../../../UI/NavigationMenu"
+import { Link } from "react-router-dom";
+
+const wallets: string[] = [
+    "0x20b4fb2929cfBe8b002b8A0c572551F755e54aEF",
+    "0x20b4fb2929cfBe8b002b8A0c572551F755e54aEF",
+    "0x20b4fb2929cfBe8b002b8A0c572551F755e54aEF",
+    "0x20b4fb2929cfBe8b002b8A0c572551F755e54aEF",
+]
+
+const NavBar = observer(() => {
+    return (
+        <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger>Wallets</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <ul className="flex flex-col gap-1 p-4 w-full">
+                            {wallets.map((wallet, idx) => (
+                                <span key={idx} className="cursor-pointer hover:bg-gray-900 p-1 rounded">
+                                    {wallet}
+                                </span>
+                            ))}
+                            <span className="flex w-full justify-center py-2 bg-gray-800 cursor-pointer hover:bg-gray-900 m-auto rounded">
+                                + Create or import an account
+                            </span>
+                        </ul>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link to={"/create-token"}>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Create Token
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <Link to={""}>
+                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Settings
+                        </NavigationMenuLink>
+                    </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+});
+
+export default NavBar;
