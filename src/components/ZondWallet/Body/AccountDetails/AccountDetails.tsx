b7b434e72a7197c163e999bdf391865454dd8cc5
moscowchill <harald@langmar.nl>
2025-03-13 10:27:41 +0100

    fix: spacing difference between the AccountDetails and CreateToken page

--- src/components/ZondWallet/Body/AccountDetails/AccountDetails.tsx
+++ src/components/ZondWallet/Body/AccountDetails/AccountDetails.tsx
@@ -175,7 +175,7 @@ const AccountDetails = observer(() => {
   }
 
   return (
-    <div className="flex w-full items-start justify-center pt-16">
+    <div className="flex w-full items-start justify-center py-8">
       <div className="relative w-full max-w-2xl px-4">
         <img
           className="fixed left-0 top-0 -z-10 h-96 w-96 -translate-x-8 scale-150 overflow-hidden opacity-10"
