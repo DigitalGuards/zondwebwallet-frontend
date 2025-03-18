d33c8b29111e3f2bd9c83f70d56b7047e81d67db
Hiroyuki <hiroyukikumazawa.jp@gmail.com>
2025-03-18 23:50:34 +0900

    fix: responsive for account list page

--- src/components/ZondWallet/Body/AccountList/AccountBalance/AccountBalance.tsx
+++ src/components/ZondWallet/Body/AccountList/AccountBalance/AccountBalance.tsx
@@ -1,18 +1,20 @@
 import { useStore } from "../../../../../stores/store";
 import { observer } from "mobx-react-lite";
 import { formatBalance } from "@/utilities/helper";
+import { cn } from "@/lib/utils";
 
 type AccountBalanceProps = {
   accountAddress: string;
+  className?: string;
 };
 
-export const AccountBalance = observer(({ accountAddress }: AccountBalanceProps) => {
+export const AccountBalance = observer(({ accountAddress, className }: AccountBalanceProps) => {
   const { zondStore } = useStore();
   const { getAccountBalance } = zondStore;
   const balance = getAccountBalance(accountAddress);
 
   return (
-    <div className="text-sm text-secondary">
+    <div className={cn("text-sm text-secondary", className)}>
       Balance: {formatBalance(balance)} QRL
     </div>
   );
