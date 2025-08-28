f8e6243a6d1ce3680daab8a1f9b3c464387ecc9c
moscowchill <harald@langmar.nl>
2025-08-28 22:08:29 +0200

    fix: clear KNOWN_TOKEN_LIST to remove deprecated token data

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,15 +6,7 @@ interface TokenInterface {
     decimals: number;
 }
 
-const KNOWN_TOKEN_LIST: TokenInterface[] = [
-    {
-        name: "DigitalGuards",
-        symbol: "DG",
-        address: "Ze751329662b34456f14c5e5be04b06f40fbee96a",
-        amount: "0",
-        decimals: 3
-    }
-]
+const KNOWN_TOKEN_LIST: TokenInterface[] = []
 
 export { KNOWN_TOKEN_LIST }
 export type { TokenInterface }
